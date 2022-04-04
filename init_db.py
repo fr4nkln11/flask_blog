283193058e40292dc9d13387fc5526a806a9d612
fr4nkl1n-1k3h <ikehfranklind3c0d3r@gmail.com>
2022-04-04 20:55:20 +0100

    added a database for blog posts

---
+++ init_db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content fot the first post')
+            )
+
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Second Post', 'Content fot the second post')
+            )
+
+connection.commit()
+connection.close()
